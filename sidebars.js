5edec5f34d5bb9ef6e982745194bacab11f7c47f
serinuntius <wasab1gj@gmail.com>
2020-07-31 18:10:55 +0900

    custom sidebar

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,12 @@
 module.exports = {
   someSidebar: {
-    Docusaurus: ['doc1', 'doc2', 'doc3'],
-    Features: ['mdx'],
+    入門: [
+      "getting-started/quick-tour",
+      "getting-started/how-it-works",
+      "getting-started/register-a-developer-account",
+      "getting-started/download-the-sdk",
+      "getting-started/install-the-unity-sdk",
+      "getting-started/place-an-ad",
+    ],
   },
 };
